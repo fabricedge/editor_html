0b71e805c19533fe3ae52e29c412150718e99bab
Sammy <samucalerahabbo@gmail.com>
2025-10-07 14:11:40 -0300

    first comit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import Link from 'next/link'
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -27,6 +27,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+           <nav>
+          {/* Prefetched when the link is hovered or enters the viewport */}
+          <Link href="/pg">Pages</Link>
+          <Link href="/pg">Pages</Link>
+          {/* No prefetching */}
+        </nav>
         {children}
       </body>
     </html>
