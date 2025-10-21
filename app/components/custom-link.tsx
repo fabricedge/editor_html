b8f023cd567bbe97de3e0cee59307831a9add0f0
Samuel <samucalerahabbo@gmail.com>
2025-10-21 14:13:41 -0300

    some files back

---
+++ app/components/custom-link.tsx
@@ -0,0 +1,40 @@
+
+import { ExternalLink } from "lucide-react"
+import Link from "next/link"
+
+interface CustomLinkProps extends React.LinkHTMLAttributes<HTMLAnchorElement> {
+  href: string
+}
+
+const CustomLink = ({
+  href,
+  children,
+  className,
+  ...rest
+}: CustomLinkProps) => {
+  const isInternalLink = href.startsWith("/")
+  const isAnchorLink = href.startsWith("#")
+
+  if (isInternalLink || isAnchorLink) {
+    return (
+      <Link href={href} className={className} {...rest}>
+        {children}
+      </Link>
+    )
+  }
+
+  return (
+    <Link
+      href={href}
+      target="_blank"
+      rel="noopener noreferrer"
+      className={"inline-flex items-center gap-1 align-baseline underline underline-offset-4"}
+      {...rest}
+    >
+      <span>{children}</span>
+      <ExternalLink className="ml-0.5 inline-block h-4 w-4" />
+    </Link>
+  )
+}
+
+export default CustomLink
