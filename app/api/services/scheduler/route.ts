cd044546950116f890ce4885ec5f9d6233c71226
Samuel <samucalerahabbo@gmail.com>
2025-11-01 22:18:40 -0300

    added vercel cron jobs

--- app/api/services/scheduler/route.ts
+++ app/api/services/scheduler/route.ts
@@ -5,29 +5,18 @@ import { db } from "../../../lib/db";
 import { pagesTable } from "../../../lib/schema";
 // var cron = require('node-cron');
 import cron from 'node-cron';
-export async function POST() {
+export async function GET() {
 
     try {
 
-        cron.schedule("0 45 */7 * *", async () => {
         console.log("");
         console.log("######################################");
         console.log("#                                    #");
-        console.log("# Running scheduler every 7 days     #");
+        console.log("# running at 06:15:00 every day    #");
         console.log("#                                    #");
         console.log("######################################");
         console.log("");
 
-        // // Example: check db for removable content and remove it
-        // const page = await db
-        //     .select()
-        //     .from(pagesTable)
-        //     .leftJoin(comments, eq(pagesTable.id, comments.post_id))
-        //     .where(eq(pagesTable.nanoid, "yourNanoIdHere"))
-        //     .limit(1);
-
-        // console.log("Checked page:", page);
-        });
 
         return NextResponse.json({ data: 'Success', status: 200 });
 
